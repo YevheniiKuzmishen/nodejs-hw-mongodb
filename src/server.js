fc2ec5dcb37281e2fa578e7eefad7d6c93bcd641
YevheniiKuzmishen <yevhenii.kuzmishen@gmail.com>
2024-09-08 13:51:10 +0200

    cleanup

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import pino from 'pino-http';
 import { env } from './utils/env.js';
 import { contactsRouter } from './routers/contacts.js';
-// import * as contactServices from './services/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
@@ -22,48 +21,9 @@ export const setupServer = () => {
 
   app.use(contactsRouter);
 
-  // app.get('/contacts', async (req, res) => {
-  //   const data = await contactServices.getAllContacts();
-
-  //   res.json({
-  //     status: 200,
-  //     message: 'Successfully found contacts',
-  //     data,
-  //   });
-  // });
-
-  // app.get('/contacts/:id', async (req, res) => {
-  //   const { id } = req.params;
-  //   const data = await contactServices.getContactById(id);
-
-  //   if (!data) {
-  //     return res.status(404).json({
-  //       message: `Contact with ID ${id} not found`,
-  //     });
-  //   }
-
-  //   res.json({
-  //     status: 200,
-  //     message: `Contact with ${id} successfully find`,
-  //     data,
-  //   });
-  // });
-
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
-
-  // app.use((req, res) => {
-  //   res.status(404).json({
-  //     message: `${req.url} not found`,
-  //   });
-  // });
-
-  // app.use((error, req, res, next) => {
-  //   res.status(500).json({
-  //     message: error.message,
-  //   });
-  // });
 
   const port = Number(env('PORT', 3000));
 
