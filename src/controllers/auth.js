2f70d296031fa801d3f61aa7c7ef603d9a3f7c95
YevheniiKuzmishen <yevhenii.kuzmishen@gmail.com>
2024-09-20 23:18:51 +0200

    add hash

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,3 @@
-// import createHttpError from 'http-errors';
 import { signup } from '../services/auth.js';
 
 export const signupController = async (req, res) => {
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,20 @@
 import { UserCollection } from '../db/models/User.js';
+import createHttpError from 'http-errors';
+import bcrypt from 'bcrypt';
 
 export const signup = async (payload) => {
-  const user = await UserCollection.create(payload);
+  const { email, password } = payload;
+  const user = await UserCollection.findOne({ email });
+  if (user) {
+    throw createHttpError(409, 'Email already exist');
+  }
 
-  return user;
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const data = await UserCollection.create({
+    ...payload,
+    password: hashPassword,
+  });
+  delete data._doc.password;
+  return data._doc;
 };
