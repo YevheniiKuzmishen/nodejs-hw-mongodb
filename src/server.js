cfd64e9ee33e5f807942f37b6424ddf681fb9feb
YevheniiKuzmishen <yevhenii.kuzmishen@gmail.com>
2024-10-06 16:16:11 +0200

    add swagger

--- src/server.js
+++ src/server.js
@@ -1,23 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-// import pino from 'pino-http';
+import pino from 'pino-http';
 import { env } from './utils/env.js';
 import { contactsRouter } from './routers/contacts.js';
 import { authRouter } from './routers/auth.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 export const setupServer = () => {
   const app = express();
 
-  // const logger = pino({
-  //   transport: {
-  //     target: 'pino-pretty',
-  //   },
-  // });
+  const logger = pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  });
 
-  // app.use(logger);
+  app.use(logger);
   app.use(cors());
   app.use(express.json());
   app.use(cookieParser());
@@ -25,6 +26,7 @@ export const setupServer = () => {
 
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
+  app.use('/api-docs', ...swaggerDocs());
 
   app.use('*', notFoundHandler);
 
